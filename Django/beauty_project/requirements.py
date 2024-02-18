564954489f7d60efd7171de9c749d2f33bc858f1
boburjon <allayorovbobur22@gmail.com>
2024-02-18 23:13:52 +0500

    Final product

---
+++ Django/beauty_project/requirements.py
@@ -0,0 +1,14 @@
+asgiref==3.7.2
+certifi==2023.7.22
+charset-normalizer==3.3.2
+crispy-bootstrap5==2023.10
+Django==4.2.6
+django-crispy-forms==2.1
+django-debug-toolbar==4.2.0
+django-jazzmin==2.6.0
+idna==3.4
+Pillow==10.1.0
+requests==2.31.0
+sqlparse==0.4.4
+typing_extensions==4.8.0
+urllib3==2.0.7
